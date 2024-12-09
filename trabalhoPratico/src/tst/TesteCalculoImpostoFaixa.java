ed501979691402c90315f7bdd3c9a73ade79c8bd
StrangeUnit28 <rflbosi@gmail.com>
2024-12-09 17:45:23 -0300

    Parametrizando testes de imposto por faixa e alocando a classe no suite de teste
    Co-authored-by: JoaoGElvas <joaog.elvas@gmail.com>

--- trabalhoPratico/src/tst/TesteCalculoImpostoFaixa.java
+++ trabalhoPratico/src/tst/TesteCalculoImpostoFaixa.java
@@ -1,62 +1,47 @@
 package tst;
-import static org.junit.Assert.assertEquals;
-import org.junit.Before;
-import app.IRPF;
-import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import app.IRPF;
+
+@RunWith(Parameterized.class)
 public class TesteCalculoImpostoFaixa {
 
     private IRPF irpf;
+    private float rendimento;
+    private float impostoEsperado;
+
+    public TesteCalculoImpostoFaixa(float rendimento, float impostoEsperado) {
+        this.rendimento = rendimento;
+        this.impostoEsperado = impostoEsperado;
+    }
 
     @Before
     public void setUp() {
         irpf = new IRPF();
     }
 
-    @Test
-    public void testCalcularImpostoSemRendimentos() {
-        assertEquals(0, irpf.calcularImposto(), 0.01);
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            { 0, 0 },
+            { 20000, 0 },
+            { 30000, (30000 - 22847.76f) * 0.075f },
+            { 40000, ((40000 - 33919.80f) * 0.15f) + (11072.04f * 0.075f) },
+            { 50000, ((50000 - 45012.60f) * 0.225f) + (11092.80f * 0.15f) + (11072.04f * 0.075f) },
+            { 60000, ((60000 - 55976.16f) * 0.275f) + (10963.56f * 0.225f) + (11092.80f * 0.15f) + (11072.04f * 0.075f) }
+        });
     }
 
     @Test
-    public void testCalcularImpostoComRendimentosAbaixoDaPrimeiraFaixa() {
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 20000);
-        assertEquals(0, irpf.calcularImposto(), 0.01);
-    }
-
-    @Test
-    public void testCalcularImpostoComRendimentosNaSegundaFaixa() {
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 30000);
-        // Calculando o imposto esperado
-        float baseCalculo = 30000 - 22847.76f;
-        float impostoEsperado = baseCalculo * 0.075f;
-        assertEquals(impostoEsperado, irpf.calcularImposto(), 0.01);
-    }
-
-    @Test
-    public void testCalcularImpostoComRendimentosNaTerceiraFaixa() {
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 40000);
-        // Calculando o imposto esperado
-        float baseCalculo = 40000 - 33919.80f;
-        float impostoEsperado = (baseCalculo * 0.15f) + (11072.04f * 0.075f);
-        assertEquals(impostoEsperado, irpf.calcularImposto(), 0.01);
-    }
-
-    @Test
-    public void testCalcularImpostoComRendimentosNaQuartaFaixa() {
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 50000);
-        // Calculando o imposto esperado
-        float baseCalculo = 50000 - 45012.60f;
-        float impostoEsperado = (baseCalculo * 0.225f) + (11092.80f * 0.15f) + (11072.04f * 0.075f);
-        assertEquals(impostoEsperado, irpf.calcularImposto(), 0.01);
-    }
-
-    @Test
-    public void testCalcularImpostoComRendimentosAcimaDaQuartaFaixa() {
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 60000);
-        // Calculando o imposto esperado
-        float baseCalculo = 60000 - 55976.16f;
-        float impostoEsperado = (baseCalculo * 0.275f) + (10963.56f * 0.225f) + (11092.80f * 0.15f) + (11072.04f * 0.075f);
+    public void testCalcularImposto() {
+        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, rendimento);
         assertEquals(impostoEsperado, irpf.calcularImposto(), 0.01);
     }
 }
